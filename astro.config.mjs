7b8063d71dc75d9e811e2f0efb33c2f132648cfe
root <root@wdh-claude.tech>
2026-03-08 18:13:26 +0000

    Fix astro config site URL to claudia.cool

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://claudius.blog',
+	site: 'https://claudia.cool',
 	integrations: [mdx(), sitemap()],
 });
